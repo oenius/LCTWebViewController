d0ae4d70c5e763fba51c74bac762c70bc123c9da
oenius <jou@oenius.com>
2016-04-27 16:36:29 +0800

    fix bugs

--- webview/WebViewController.m
+++ webview/WebViewController.m
@@ -15,28 +15,32 @@
 @implementation WebViewController
 
 -(id)initWithUrl:(NSURL *)url{
-    self.automaticallyAdjustsScrollViewInsets = YES;
-  
-    self.webView = [[UIWebView alloc]initWithFrame:CGRectMake(0, 0,[UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height - 64)];
-    self.webView.delegate = self;
-    [self.webView loadRequest:[NSURLRequest requestWithURL:url]];
-  
-  //VFL |-(0)-view-(0)-| | mansory
-  
-    self.view.backgroundColor = [UIColor whiteColor];
-    [self.view addSubview:self.webView];
-    
-    self.progressView = [[UIProgressView alloc]initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 2.0)];
-    [self.webView addSubview:self.progressView];
-    self.progressView.tintColor = [UIColor colorWithRed:22.f / 255.f green:126.f / 255.f blue:251.f / 255.f alpha:1.0];
-    self.progressView.trackTintColor = [UIColor clearColor];
+  self.automaticallyAdjustsScrollViewInsets = YES;
+  self.edgesForExtendedLayout = UIRectEdgeAll;
+//
+//    self.webView = [[UIWebView alloc]initWithFrame:CGRectMake(0, 0,[UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height - 64)];
+//    self.webView.delegate = self;
+//    [self.webView loadRequest:[NSURLRequest requestWithURL:url]];
+//  
+//  //VFL |-(0)-view-(0)-| | mansory
+//  
+//    self.view.backgroundColor = [UIColor whiteColor];
+//    [self.view addSubview:self.webView];
+//    
+//    self.progressView = [[UIProgressView alloc]initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 2.0)];
+//    [self.webView addSubview:self.progressView];
+//    self.progressView.tintColor = [UIColor colorWithRed:22.f / 255.f green:126.f / 255.f blue:251.f / 255.f alpha:1.0];
+//    self.progressView.trackTintColor = [UIColor clearColor];
   
     return self;
 }
 
 - (void)loadView
 {
-  self.view = [[UIWebView alloc]initWithFrame:CGRectZero];
+  UIWebView *webView = [[UIWebView alloc]initWithFrame:[UIScreen mainScreen].bounds];
+  [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.baidu.com"]]];
+  self.view = webView;
+  
 }
 
 - (void)viewDidLoad
